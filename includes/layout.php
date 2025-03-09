7c0a1815bcb60c8ca5e06dfded0e9c83b9271843
suthikawong <suthika.wsrd@gmail.com>
2025-03-09 20:30:16 +0000

    create navigation bar

--- includes/layout.php
+++ includes/layout.php
@@ -5,12 +5,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=\, initial-scale=1.0">
   <title><?php echo $title; ?></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../public/assets/css/style.css">
+  <link rel="stylesheet" href="../public/assets/css/layout.css">
+  <link rel="stylesheet" href="../public/assets/css/nav.css">
 </head>
 
 <body>
-  <?php require '../includes/header.php'; ?>
   <?php require '../includes/nav.php'; ?>
-  <main>
+  <main id='main-container'>
     <?php
     echo $content;
     ?>
--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,41 @@
 <nav>
-  <ul>
-    <li><a href="/event-management/public/index.php">Home</a></li>
-    <li><a href="/event-management/pages/about.php">About</a></li>
-  </ul>
+  <div class="desktop-nav-container">
+    <h1>Logo</h1>
+    <div class="menu-container">
+      <ul class="menu-list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+      <div class="profile-container">
+        <img class="profile-img" src="../public/assets/images/profile.png" alt="profile" class="profile">
+        <div class="profile-name">Sophia Rose</div>
+      </div>
+    </div>
+  </div>
+  <div class="mobile-nav-container">
+    <button class="ham-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#sideMenu">
+      <i class="fa fa-bars"></i>
+    </button>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="sideMenu">
+      <div class="offcanvas-header">
+        <div class="menu-header-container">
+          <!-- <h5 class="offcanvas-title" id="sideMenuLabel">Offcanvas</h5> -->
+          <div class="profile-container">
+            <img class="profile-img" src="../public/assets/images/profile.png" alt="profile" class="profile">
+            <div class="profile-name">Sophia Rose</div>
+          </div>
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"></button>
+        </div>
+      </div>
+      <div class="divider"></div>
+      <div class="offcanvas-body">
+        <ul class="menu-list">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </nav>
